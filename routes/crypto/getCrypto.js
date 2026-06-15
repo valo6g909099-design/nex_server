87cc4c250a05a5d0845982d252ce5ee4944ec96e
valo6g909099-design <valo6g909099@gmail.com>
2026-06-15 21:16:02 +0600

    Update getCrypto.js

--- routes/crypto/getCrypto.js
+++ routes/crypto/getCrypto.js
@@ -22,6 +22,11 @@ async function getCryptoItems(req, res) {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     console.log(decoded)
     const cryptoItemsData = await getCryptousersByEmail(decoded.email);
+
+  console.log(cryptoItemsData)
+
+
+    
     const result = await getProtfolio(cryptoItemsData["crypto"]);
     return res.status(200).json({ success: true, data: result });
   } catch (error) {
