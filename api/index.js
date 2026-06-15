46859b41f8102d63e2376d62849964d307b82ec3
valo6g909099-design <valo6g909099@gmail.com>
2026-06-15 17:36:33 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -10,6 +10,8 @@ app.use(
   cors({
     origin: "http://localhost:4200",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 app.use(express.json());
